f51ec177f18f9a057e22f06060d12b7ce2dbf255
tuan.da@geekup.vn <tuan.da@geekup.vn>
2024-02-27 10:35:27 +0000

    [v2.1.0] migrate version project

--- scripts/pre-bootstrap.sh
+++ scripts/pre-bootstrap.sh
@@ -2,10 +2,6 @@
 echo "Installing fvm..."
 dart pub global activate fvm
 echo "Installing Melos..."
-dart pub global activate melos 3.0.1
+dart pub global activate melos 4.1.0
 echo "Installing FlutterGen..."
 dart pub global activate flutter_gen
-echo "Installing Lefthook..."
-dart pub global activate lefthook
-echo "Installing Dart code metrics..."
-dart pub global activate dart_code_metrics
